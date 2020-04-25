7470f5f36512f6cad0e481f6ab8cc517e7ef1cdb
andrebatist <arsp93@gmail.com>
2020-04-25 12:10:37 +0300

    Bridge structural pattern

---
+++ src/ru/aplaksin/designpatterns/structural/bridge/CppDeveloper.java
@@ -0,0 +1,8 @@
+package ru.aplaksin.designpatterns.structural.bridge;
+
+public class CppDeveloper implements Developer{
+    @Override
+    public void writeCode() {
+        System.out.println("C++ developer writes C++ code...");
+    }
+}
---
+++ src/ru/aplaksin/designpatterns/structural/bridge/Developer.java
@@ -0,0 +1,5 @@
+package ru.aplaksin.designpatterns.structural.bridge;
+
+public interface Developer {
+    public void writeCode();
+}
